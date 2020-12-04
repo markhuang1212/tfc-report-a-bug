dfc4ee5be97963aaab1db1a2e6b86b6c0e230f73
hm <mark.huang1212@gmail.com>
2020-12-04 19:34:13 +0800

    ts

--- src/AppContent.tsx
+++ src/AppContent.tsx
@@ -1,33 +1,38 @@
 import './AppContent.css'
 import { useState } from 'react'
+import React from 'react'
 
 function AppContent() {
     const [submitName, setSubmitName] = useState('')
     const [submitEmail, setSubmitEmail] = useState('')
     const [submitDescription, setSubmitDescription] = useState('')
 
-    return (
+    const submitOnClick = (e:React.MouseEvent<HTMLInputElement,MouseEvent>) => {
+        e.preventDefault()
+        console.log("Hello!")
+    }
 
+    return (
         <div className="AppContent">
             <div className="AppContentFormContainer">
                 <form className="AppContentForm">
                     <div className="AppContentFormTextField">
                         <label>Name:</label>
-                        <input type="name" />
+                        <input type="name" name="nameField" />
                     </div>
                     <div className="AppContentFormBugDescriptionField">
                         <label>Bug Description:</label>
-                        <textarea placeholder="Describe Your Bug Here. Please provide as much detail as possible as it will help us improve our product better."></textarea>
+                        <textarea placeholder="Describe Your Bug Here. Please provide as much detail as possible as it will help us improve our product better." name="descriptionField" />
                     </div>
-                    <div className="AppContentFormTextField">
+                    <div className="AppContentFormSelectFileField">
                         <label>Screenshot(optional):</label>
-                        <input type="file" accept="image/*" />
+                        <input type="file" accept="image/*" name="screenshotField" />
                     </div>
                     <div className="AppContentFormTextField">
                         <label>Email(optional):</label>
-                        <input type="email" />
+                        <input type="email" name="emailField" />
                     </div>
-                    <input type="submit" className="AppContentFormSubmitButton" />
+                    <input type="submit" className="AppContentFormSubmitButton" onClick={submitOnClick}/>
                 </form>
             </div>
         </div >
